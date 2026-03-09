2cff9b26363fb0c491bb88aa52cbc5841bf75963
mark-dev <markmudyiwah0@gmail.com>
2026-03-09 10:28:43 +0200

    Added two more routes

--- scripts/data/routes.js
+++ scripts/data/routes.js
@@ -14,7 +14,7 @@ const southernSuburbs = [
   "Grassy Park", "Lotus River", "Pelican Park", "Zeekoevlei", "Muizenberg", "Marina da Gama","Fisherman's Village",
   "Capricorn", "St James", "Kalk Bay", "Fish Hoek", "Sunny Cove", "Glencairn", "Simons Town", 
   "Welcome Glen", "Noordhoek", "Sun Valley", "Capri", "Kommetjie", "Ocean View", "Masiphumelele", 
-  "Scarborough", "Red Hill","Belthorn Estate"
+  "Scarborough", "Red Hill","Belthorn Estate","Newfields","Ottery"
 ];
 
 const northernSuburbs = [
